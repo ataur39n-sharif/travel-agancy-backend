b18605227222287ed56ff54a24dd0ad02b5dbca8
Ataur Rahman <rahman.sharif39n@gmail.com>
2023-10-19 11:51:17 +0600

    migration file added

---
+++ prisma/migrations/20231019042606_images_property_removed_from_tour_package_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `images` on the `tour_packages` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tour_packages" DROP COLUMN "images";
